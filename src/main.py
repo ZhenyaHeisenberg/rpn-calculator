409baf1cfb5fa5e45220f58ac199889b0564dbb9
Hahkov Roman <rsshashkov@mai.education>
2025-10-05 11:49:28 +0300

    add src to directory

---
+++ src/main.py
@@ -0,0 +1,129 @@
+ex = input()
+
+operators = "+-*/%&^"
+
+def parse(ex):
+
+    ex = ex.replace("//", "&")
+    ex = ex.replace("(", " ( ")
+    ex = ex.replace(")", " ) ")
+
+    if ex.count("(") != ex.count(")"):
+        return "Error_1" #Ошибка: неравное колличество символов "(" и ")"
+    else:
+        ex = ex.replace("(", "")
+        ex = ex.replace(")", "")
+
+        
+
+    
+    
+    
+        
+
+    s = ex.split()
+
+    if len(s) != (2 * (s.count("+") + s.count("-") + s.count("*") + s.count("/") + s.count("%") + s.count("&") + s.count("^")) + s.count("~")) + 1: #(кол-во бинарных операторов) * 2 + кол-во унарных операторов + 1 == кол-во элементов в стеке (условие правильности записи операторов и операндов в обратной польсокй нотации) 
+        return "Error_2" #Ошибка ввода операторов и операндов
+
+    
+    return s
+
+
+
+
+result = parse(ex)
+print(result)
+
+
+
+
+"""def solve(result):
+
+    s = result
+
+    
+    while len(s) > 1:
+        for i in range(len(s)):
+
+            if s[i] == "~" and i > 0:
+                    s[i-1] = float(s[i-1]) * (-1)
+                    s.pop(i)
+                    break
+            elif s[i] == "~" and i == 0:
+                return "Ошибка ввода операторов"
+            
+            if str(s[i]) in operators and i < 2:
+                return "Ошибка ввода операторов"
+            elif str(s[i]) in operators and i >= 2:
+
+                
+                if s[i] == "+":
+                    s[i-2] = float(s[i-2]) + float(s[i-1])
+                    s.pop(i)
+                    s.pop(i-1)
+                    break
+
+                if s[i] == "-":
+                    s[i-2] = float(s[i-2]) - float(s[i-1])
+                    s.pop(i)
+                    s.pop(i-1)
+                    break
+
+                if s[i] == "*":
+                    s[i-2] = float(s[i-2]) * float(s[i-1])
+                    s.pop(i)
+                    s.pop(i-1)
+                    break
+
+                if s[i] == "/":
+                    s[i-2] = float(s[i-2]) / float(s[i-1])
+                    s.pop(i)
+                    s.pop(i-1)
+                    break
+
+                if s[i] == "%":
+                    if float(s[i-2]) % 1 == 0 and float(s[i-1]) % 1 == 0:
+                        s[i-2] = float(s[i-2]) % float(s[i-1])
+                        s.pop(i)
+                        s.pop(i-1)
+                        break
+                    else:
+                        return 'Error_3' 
+
+                if s[i] == "&":
+                    if float(s[i-2]) % 1 == 0 and float(s[i-1]) % 1 == 0:
+                        s[i-2] = float(s[i-2]) // float(s[i-1])
+                        s.pop(i)
+                        s.pop(i-1)
+                        break
+                    else:
+                        return 'Error_4' 
+
+                if s[i] == "^":
+                    s[i-2] = float(s[i-2]) ** float(s[i-1])
+                    s.pop(i)
+                    s.pop(i-1)
+                    break
+
+    return s
+
+
+if type(result) == str:
+    if result == "Error_1":
+        print('Ошибка: неравное колличество символов "(" и ")"')
+    elif result == "Error_2":
+        print('Ошибка ввода операторов и операндов')
+    elif result == "Error_3":
+        print('Ошибка: операция "%" только для целых')
+    elif result == "Error_4":
+        print('Ошибка: операция "//" только для целых')
+    
+else:
+    print(solve(result))"""
+
+
+
+
+
+
