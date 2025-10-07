ad8f60fbd72807a2594e466b62e5a79a8d60a3d4
ZhenyaHeisenberg <em.krem08@gmail.com>
2025-10-07 22:57:59 +0300

    Исправлен баг в def_check_brackets

--- tests/def_check_brackets.py
+++ tests/def_check_brackets.py
@@ -1,4 +1,7 @@
 from def_check_operators import check_operators
+from def_solve import solve
+
+s = ['1', '(', '4', '+', '4', ')', '4']
 
 def check_brackets(stack): #проверяет, верно ли расставлены скобки
 
@@ -13,9 +16,12 @@ def check_brackets(stack): #проверяет, верно ли расставл
 
         substack = stack[open_bracket_index+1:close_bracket_index]
 
-        if check_operators(substack) == 0:
-            return 'Ошибка ввода операторов и операндов'
-        else:
+        if check_operators(substack) == 1 and len(solve(substack)) == 1:
             stack.pop(close_bracket_index)
             stack.pop(open_bracket_index)
+        else:
+            return 'Ошибка ввода операторов и операндов'
+        
+            
     return stack
+print(check_brackets(s))
