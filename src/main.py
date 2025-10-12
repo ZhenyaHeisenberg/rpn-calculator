d6205e311cfe560717d4e271463c7b7975c57c45
Roman Shashkov <em.krem08@gmail.com>
2025-10-12 16:33:07 +0300

    general fix

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-operators = "+-*/%&^"
+from constants import OPERATORS
         
     
-def parse(ex):
+def parse(ex: str) -> list[str] | str:
+    """принимает на вход строку - выражение,
+    значение которого необходимо вычислить"""
+
     
     """Функция приводит строку к правильному виду
-        и создает из нее стек"""
+    и создает из нее стек"""
 
     brackets_count = 0
     for i in range(len(ex)):
@@ -25,11 +28,13 @@ def parse(ex):
     
 
     stack = ex.split() # (1)
-     
+    
     return stack
 
 
-def check_brackets(stack):
+def check_brackets(stack: list[str]) -> list[str] | str:
+    """принимает на вход список - результат функции parse().
+    Если есть скобки - проверяет правильность их использования"""
     
     """Путем нахождения последнего символа ')' и предшествующего ему
     символа '(', выбирает скобки с высшим приоритетом.
@@ -45,30 +50,38 @@ def check_brackets(stack):
 
         substack = stack[open_bracket_index+1:close_bracket_index]
 
-        if check_operators(substack) == 0:
+        if check_operators(substack) == False:
             return 'Ошибка ввода операторов и операндов'
         else:
             stack.pop(close_bracket_index)
             stack.pop(open_bracket_index)
     return stack
 
-def check_operators(substack):
+def check_operators(substack: list[str]) -> bool:
     
-    """Проверяет правильность ввода операторов и операндов
-    в соответствии с формулой, описанной в README"""
+    """принимает на вход список - результат функции check_brackets().
+    В соответствии описанной в README формуле проверяет
+    правильность введенного выражения
+    Выдает результат True если выражение простороено верно
+    Выдает результат False если выражение простороено неверно"""
+    
     
     if len(substack) != (2 * (substack.count("+") + substack.count("-") + substack.count("*") + substack.count("/") + substack.count("%") + substack.count("&") + substack.count("^")) + substack.count("~") + 1):
-        return 0
+        return False
     else:
-        return 1
+        return True
 
-def solve(checked_stack):
+def solve(checked_stack: list[str]) -> list[str] | str:
+
+    """принимает на вход список - результат функции check_brackets().
+    Для проверки корректности выражения использует check_operators()"""
+    
 
     """Находит первое вхождение оператора в стеке, после чего применяет его
-    предыдущий/предыдущийе операнд/операнды, записывает результат в стек,
-    а отработавшые символы из стека удаляет"""
+    на предыдущий/предыдущийе операнд/операнды, записывает результат в стек,
+    а отработавшие символы из стека удаляет"""
 
-    if check_operators(checked_stack) == 0:
+    if check_operators(checked_stack) == False:
         return 'Ошибка ввода операторов и операндов'
 
     s = checked_stack
@@ -77,48 +90,49 @@ def solve(checked_stack):
         for i in range(len(s)):
 
             if s[i] == "~" and i > 0: # (2)
-                s[i-1] = float(s[i-1]) * (-1)
+                s[i-1] = str(float(s[i-1]) * (-1))
                 s.pop(i)
                 break
             elif s[i] == "~" and i == 0:
                 return "Ошибка ввода операторов и операндов"
                 
-            if str(s[i]) in operators and i < 2: # (3)
+            if s[i] in OPERATORS and i < 2: # (3)
                     
                 return "Ошибка ввода операторов и операндов"
-            elif str(s[i]) in operators and i >= 2:
+            elif str(s[i]) in OPERATORS and i >= 2:
 
                     
                 if s[i] == "+":
-                    s[i-2] = float(s[i-2]) + float(s[i-1])
+                    s[i-2] = str(float(s[i-2]) + float(s[i-1]))
                     s.pop(i)
                     s.pop(i-1)
                     break
 
                 if s[i] == "-":
-                    s[i-2] = float(s[i-2]) - float(s[i-1])
+                    s[i-2] = str(float(s[i-2]) - float(s[i-1]))
                     s.pop(i)
                     s.pop(i-1)
                     break
 
                 if s[i] == "*":
-                    s[i-2] = float(s[i-2]) * float(s[i-1])
+                    s[i-2] = str(float(s[i-2]) * float(s[i-1]))
                     s.pop(i)
                     s.pop(i-1)
                     break
 
                 if s[i] == "/":
-                    if float(s[i-1]) != 0:
-                        s[i-2] = float(s[i-2]) / float(s[i-1])
+                    if float(s[i-1]) == 0:
+                        return 'Ошибка: деление на 0 невозможно\n\n\n'
+                    else:
+                        s[i-2] = str(float(s[i-2]) / float(s[i-1]))
                         s.pop(i)
                         s.pop(i-1)
                         break
-                    else:
-                        return 'Ошибка: деление на 0 невозможно\n\n\n' 
+                         
 
                 if s[i] == "%":
                     if float(s[i-2]) % 1 == 0 and float(s[i-1]) % 1 == 0:
-                        s[i-2] = float(s[i-2]) % float(s[i-1])
+                        s[i-2] = str(float(s[i-2]) % float(s[i-1]))
                         s.pop(i)
                         s.pop(i-1)
                         break
@@ -127,7 +141,7 @@ def solve(checked_stack):
 
                 if s[i] == "&":
                     if float(s[i-2]) % 1 == 0 and float(s[i-1]) % 1 == 0:
-                        s[i-2] = float(s[i-2]) // float(s[i-1])
+                        s[i-2] = str(float(s[i-2]) // float(s[i-1]))
                         s.pop(i)
                         s.pop(i-1)
                         break
@@ -135,7 +149,7 @@ def solve(checked_stack):
                         return 'Ошибка: операция "//" только для целых\n\n\n' 
 
                 if s[i] == "^":
-                    s[i-2] = float(s[i-2]) ** float(s[i-1])
+                    s[i-2] = str(float(s[i-2]) ** float(s[i-1]))
                     s.pop(i)
                     s.pop(i-1)
                     break
@@ -147,25 +161,33 @@ def solve(checked_stack):
 
 
 
-def calc(ex):
+def calc(ex: str) -> str:
+
+    """принимает на вход - результат функции check_brackets().
+    Для проверки корректности выражения использует check_operators()"""
+    
     
     """Сочетает в себе все служебные функции, calc(ввод)
     напрямую выдает результат, опираясь на служебные функции"""
- 
-    if type(parse(ex)) == str:
+
+    
+
+
+    if isinstance(parse(ex), str):
         return stack
     else:
         checked_stack = check_brackets(parse(ex))
 
-        if type(checked_stack) == str:
+        if isinstance(checked_stack, str):
             return checked_stack
         else:
             return solve(checked_stack)[0]
 
+        
 
-while True:
-    ex = input("Введите выражение: \n")
-    print("\n", calc(ex), "\n\n", sep="")
+
+ex = input("Введите выражение: \n")
+print("\n", calc(ex), "\n\n", sep="")
 
 
     
