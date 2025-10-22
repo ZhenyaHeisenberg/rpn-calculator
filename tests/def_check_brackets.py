a26a00ef1260be633ee78ef531cb52f18e46b699
Roman Shashkov <em.krem08@gmail.com>
2025-10-22 23:13:36 +0300

    ruff debug

--- tests/def_check_brackets.py
+++ tests/def_check_brackets.py
@@ -2,12 +2,6 @@ from def_check_operators import check_operators
 from def_solve import solve
 
 def check_brackets(stack: list[str]) -> list[str] | str:
-
-    """Путем нахождения последнего символа ')' и предшествующего ему
-    символа '(', выбирает скобки с высшим приоритетом.
-    Проверяет, корректно ли выражение в скобках, применяя на него функцию solve().
-    если выражение корректно, удаляет скобки из стека"""
-    
             
     while ")" in stack:
         close_bracket_index = stack.index(")")
@@ -17,9 +11,9 @@ def check_brackets(stack: list[str]) -> list[str] | str:
 
         substack = stack[open_bracket_index+1:close_bracket_index]
 
-        if check_operators(substack):
+        if check_operators(substack) == False:
+            return 'Ошибка ввода операторов и операндов'
+        else:
             stack.pop(close_bracket_index)
             stack.pop(open_bracket_index)
-        return 'Ошибка ввода операторов и операндов'
-        
     return stack
